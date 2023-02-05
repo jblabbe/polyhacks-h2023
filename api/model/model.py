dd0cbd7a92339a83bac9af5970d31e75fa7640de
jblabbe <jb.labbe@hotmail.com>
2023-02-04 23:28:59 -0500

    Changed data processing

--- api/model/model.py
+++ api/model/model.py
@@ -3,34 +3,61 @@ import numpy as np
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import matplotlib.pyplot as plt
-
+from datetime import datetime
 import json
 
 
-def get_coefficients(history):
+def get_actions(history):
 
     processed = preprocess_data(history)
 
-    model = smf.glm(formula = 'mood ~ exercise+sleep+screentime', data=processed)
-    result = model.fit()
+    actions_info = process_quadrant_analysis(processed)
 
-    coefficients = result.params.to_dict()
-    coefficients.pop('Intercept')
-    return coefficients
+    return actions_info
 
 def preprocess_data(raw_data):
     formatted_data = {
+        'mood': [],
+        'exercise': [],
+        'sleep': [],
+        'screentime': [],
+        'date': [],
     }
 
     for element in raw_data:
-        print(element['date'])
-        row = []
-        row.append(element['mood'])
-        row.append(element['daily']['exercise'])
-        row.append(element['daily']['sleep'])
-        row.append(element['daily']['screentime'])
-        formatted_data[element['date']] = row
+        formatted_data['mood'].append(element['mood'])
+        formatted_data['exercise'].append(element['daily']['exercise'])
+        formatted_data['sleep'].append(element['daily']['sleep'])
+        formatted_data['screentime'].append(element['daily']['screentime'])
+        formatted_data['date'].append(datetime.strptime(element['date'], '%Y-%m-%d').date())
 
-    df = pd.DataFrame.from_dict(formatted_data, orient='index', columns=['mood', 'sleep', 'exercise', 'screentime'])
+    df = pd.DataFrame.from_dict(formatted_data)
 
     return df
+
+def process_quadrant_analysis(df):
+    params_cols = ['sleep', 'exercise', 'screentime']
+    df.sort_values(by=['date'], inplace=True)
+
+    moods = df['mood']
+    params = df[params_cols]
+    today = df.tail(1)
+
+    means = params.mean()
+
+    diff = today[params_cols] - means
+
+    actions = diff.mul(moods.tail(1), axis=0)
+
+    for param in params_cols:
+        actions[f'{param}_action'] = actions[param].apply(lambda x: 1 if x > 0 else ( -1 if x < 0 else 0))
+
+    response_dict = {
+    }
+
+    for param in params_cols:
+        response_dict[f'{param}_mean'] = means[param]
+        response_dict[f'{param}_action'] = actions[f'{param}_action'].iloc[0]
+
+
+    return response_dict
