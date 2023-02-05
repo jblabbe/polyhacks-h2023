02a0a635a061fbcd043881f003b443fa55415f91
Jacob Taylor <jacob.taylor@polymtl.ca>
2023-02-05 04:30:38 -0500

    styling for login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -2,6 +2,7 @@ import { Header, Div } from '../Layout'
 import Slider from '@mui/material/Slider'
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
+import { ClassNames } from '@emotion/react';
 
 export function Login(props) {
 
@@ -11,12 +12,12 @@ export function Login(props) {
 
   return (
     <>
-    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100%", gap: "1em", padding: "0.5em"}}>
-      <label>Enter your name:</label>
-      <input type="text" onChange={(e) => userName = e.target.value}></input>
+    <Box className="background-gradient" sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100%", gap: "1em", padding: "0.5em"}}>
+      <label className="name-input-text">Enter your name</label>
+      <input className='name-input-login' type="text" onChange={(e) => userName = e.target.value}></input>
       <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "1em", padding: "0.5em"}}>
-        <button onClick={() => {props.stateChanger("moodSmiley"); props.userNameChanger(userName);}}>Login</button>
-        <button onClick={() => props.stateChanger("createForm")}>Sign up</button>
+        <button className='login-btn' onClick={() => {props.stateChanger("moodSmiley"); props.userNameChanger(userName);}}>Login</button>
+        <button className='login-btn' onClick={() => props.stateChanger("createForm")}>Sign up</button>
       </Box>
     </Box>
     </>
