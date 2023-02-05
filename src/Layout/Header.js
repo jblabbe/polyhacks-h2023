018d3b04685801a66211068056056dc4db86998d
Jacob Taylor <jacob.taylor@polymtl.ca>
2023-02-05 04:43:15 -0500

    style

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -6,8 +6,8 @@ export function Header(props) {
 
     return (
         <Box className="header-sx" sx={{height: "10%", display: "flex", alignItems: "center", justifyContent: "end"}}>
-            <Box sx={{display: "flex", flexDirection: "row", alignItems: "center", padding: "1em"}}>
-                <Avatar onClick={() => props.stateChanger("dashboard")} style={{ cursor: "pointer", marginRight: "0.5em" }}></Avatar>
+            <Box sx={{display: "flex", flexDirection: "row", alignItems: "center", padding: "1em", color: "#ffffff"}}>
+                <Avatar className="avatar" onClick={() => props.stateChanger("dashboard")} style={{ cursor: "pointer", marginRight: "0.5em" }}></Avatar>
                 {props.userName}
             </Box>
         </Box >
