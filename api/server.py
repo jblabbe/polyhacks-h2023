5c32bfed991860fcca3b9f4cf9953534d1dd2065
benjaminpiche8 <benjamin.piche@polymtl.ca>
2023-02-04 21:26:54 -0500

    started server routes

--- api/server.py
+++ api/server.py
@@ -1,8 +1,25 @@
 import time
 from flask import Flask
+from flask import request
+import databaseService as db
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/user/<id>')
+def getUser(id):
+    return { 'user': db.getUser(id) }
+
+@app.route('/user', methods=['POST'])
+def createUser():
+    # db.createUser(request.json)
+    return { 'user': "" }
+
+@app.route('/scores/<user>', methods=['PATCH'])
+def updateHistory(user):
+    # mongo service to add score to the user
+    return { 'user': "" }
+
+@app.route('/scores/<user>')
+def getHistory(user):
+    # mongo service to get scores for the user
+    return { 'user': "" }
